2b3bd40624e4626ba2b4fa115082e74142833bc8
Haruka Ichinose <hic21@uclive.ac.nz>
2022-01-14 16:15:48 +1300

    Implemented backend. Saves employees on boot. Fetches all employees on get request.

--- src/main/java/com/example/springbootbackend/SpringBootbackendApplication.java
+++ src/main/java/com/example/springbootbackend/SpringBootbackendApplication.java
@@ -1,13 +1,46 @@
 package com.example.springbootbackend;
 
+import com.example.springbootbackend.entity.Employee;
+import com.example.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootbackendApplication {
+public class SpringBootbackendApplication implements CommandLineRunner {
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootbackendApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+
+    Employee employee1 = Employee.builder()
+        .firstName("Haruka")
+        .lastName("Ichinose")
+        .email("haruka@gmail.com")
+        .build();
+
+    Employee employee2 = Employee.builder()
+        .firstName("Jeff")
+        .lastName("Bezos")
+        .email("jeff@gmail.com")
+        .build();
+
+    Employee employee3 = Employee.builder()
+        .firstName("Tim")
+        .lastName("Cook")
+        .email("tim@gmail.com")
+        .build();
+
+    employeeRepository.save(employee1);
+    employeeRepository.save(employee2);
+    employeeRepository.save(employee3);
+
+  }
 }
---
+++ src/main/java/com/example/springbootbackend/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.springbootbackend.controller;
+
+import com.example.springbootbackend.entity.Employee;
+import com.example.springbootbackend.repository.EmployeeRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  @GetMapping("/employees")
+  public List<Employee> fetchEmployees() {
+    return employeeRepository.findAll();
+
+  }
+}
